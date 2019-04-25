9ebb7e209599a05d867711f9b058037497dd7db8
megacron <megacron@d3c3p7.com>
2019-04-24 22:56:42 -0400

    may add more later--maybe

--- megamenu.sh
+++ megamenu.sh
@@ -15,8 +15,8 @@ done
 while [[ 1 ]]
 do
 	echo "$menu"
-	read -p "Enter # Selection $> "
-	#clear
+	read -p "Enter Selection, $USER [1-9] > "
+	clear
 	case $REPLY in
 		1)	sudo apt update; sudo apt upgrade -y
 			;;
@@ -25,10 +25,24 @@ do
 		3)	echo "Hostname: $HOSTNAME"
 			uptime
 			;;
-		4)	echo "Humans don't deserve to live!"
+		4)	echo "Your IP is:"
+			dig +short myip.opendns.com @resolver1.opendns.com
+			;;
+		5)	echo "These are included in your \$PATH:"
+			echo -e ${PATH//:/\\n}
+			;;	
+		6)	echo "The week is... "
+			date +%V
+			;;
+		7)	echo "The next Easter will be "
+			ncal -e
+			;;
+		8)	nc towel.blinkenlights.nl 23
+			;;	
+		9)	echo "Humans don't deserve to live!"
 			break
 			;;
-		*)	echo "Megacron has labeled you as Invalid" >&2
+		*)	echo "Megacron has labeled $USER as Invalid" >&2
 			exit 1
 			;;
 	esac
