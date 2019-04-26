3bb0abd8c02f4cce00cb094812ae372679372652
megacron <megacron@d3c3p7.com>
2019-04-25 22:15:15 -0400

    added spacing for clarity in repsponses

--- megamenu.sh
+++ megamenu.sh
@@ -21,21 +21,32 @@ do
 		1)	sudo apt update; sudo apt upgrade -y
 			;;
 		2)	df -h
+			echo ""
 			;;
-		3)	echo "Hostname: $HOSTNAME"
+		3)	echo " Hostname: $HOSTNAME"
+			echo ""
 			uptime
+			echo ""
 			;;
 		4)	echo "I know where you are:"
+			echo ""
 			dig +short myip.opendns.com @resolver1.opendns.com
+			echo ""
 			;;
 		5)	echo "Weak human, this is your \$PATH:"
+			echo ""
 			echo -e ${PATH//:/\\n}
+			echo ""
 			;;	
 		6)	echo "The week is... "
+			echo ""
 			date +%V
+			echo ""
 			;;
 		7)	echo "This year's Easter is on "
+			echo ""
 			ncal -e
+			echo ""
 			;;
 		8)	nc towel.blinkenlights.nl 23
 			;;	
@@ -45,9 +56,11 @@ do
 			arr[3]="Give me the Allspark, and you may live to be my pet."
 			rand=$(( RANDOM % 4 ))
 			echo "${arr[$rand]}"
+			echo ""
 			break
 			;;
 		*)	echo "Megacron has labeled $USER as Invalid" >&2
+			echo ""
 			exit 1
 			;;
 	esac
